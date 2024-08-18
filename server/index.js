4d61b216824444db37d25fc5aa0c6669d33cbd8f
tamilzh <tamilzhchelvan@gmail.com>
2024-08-18 22:23:52 +0530

    vercel

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 
 */
 app.get("/health", function (req, res) {
-  res.send("Welcome to Fund Raisin App");
+  res.send(process.env);
 });
 
 app.use(network);
