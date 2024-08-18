cf111585a34c9ce9c526b546702b66e6960ed9a3
tamilzh <tamilzhchelvan@gmail.com>
2024-08-18 22:15:15 +0530

    vercel

--- server/index.js
+++ server/index.js
@@ -1,15 +1,16 @@
 import "./dotent-config.js";
 import express from "express";
-/*import path from "path";
-import * as url from "url";
-import bodyParser from "body-parser";
-import cors from "cors";
+
 import artAPI from "./api/artist.js";
 import siteAPI from "./api/site.js";
 import fundraisinAPI from "./api/fundraisin.js";
 import arweaveAPI from "./api/arweave.js";
 import shareAPI from "./api/share.js";
 import network from "./api/network.js";
+/*import path from "path";
+import * as url from "url";
+import bodyParser from "body-parser";
+import cors from "cors";
 
 const __dirname = url.fileURLToPath(new URL("..", import.meta.url));
 
@@ -27,7 +28,7 @@ app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.get("/health", function (req, res) {
   res.send("Welcome to Fund Raisin App");
 });
-/*
+
 app.use("/site", siteAPI);
 app.use("/artCreationPack", artAPI);
 app.use("/fundraisin", fundraisinAPI);
@@ -45,7 +46,7 @@ app.get("/constants", async (req, res) => {
     NETWORK_URL: process.env.NETWORK_URL
   });
 });
-*/
+
 
 app.use("/", (req, res) => {
   res.send("server is  running");
